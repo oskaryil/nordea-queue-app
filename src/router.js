e3ddab82532ad64541af289f1ce1d077fb05d817
Oskar Yildiz <yildizoskar@gmail.com>
2017-11-25 15:05:27 +0200

    added temporary route for setting up the nordea oauth flow

--- src/router.js
+++ src/router.js
@@ -3,9 +3,11 @@ import { Match } from 'preact-router/match';
 import Intro from './components/introduction';
 import Home from './routes/home';
 import Profile from './routes/profile';
+import Nordea from './routes/nordea-auth';
 
 export default props =>
   <Router>
     <Intro path="/" />
     <Profile path="/profile" />
+		<Nordea path="/nordea" />
   </Router>
