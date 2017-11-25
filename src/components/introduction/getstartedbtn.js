c54cac35747279777daa883bceb0cd2dfb950b20
martinnord <martin.nordstrom99@gmail.com>
2017-11-25 16:06:52 +0200

    getstartedbtn now redirects to the nordea auth page

--- src/components/introduction/getstartedbtn.js
+++ src/components/introduction/getstartedbtn.js
@@ -1,9 +1,18 @@
-import {h, Component} from 'preact'
+import { h, Component } from "preact";
+import { connect } from "react-redux";
+import { redirectToNordea } from "../../routes/nordea-auth/actions";
 
-const GetStartedBtn = () => {
-  return (
-    <button className="getStartedBtn">Click here to get started!</button>
-  )
+class GetStartedBtn extends Component {
+  render() {
+    return (
+      <button
+        className="getStartedBtn"
+        onClick={() => this.props.redirectToNordea()}
+      >
+        Click here to get started!
+      </button>
+    );
+  }
 }
 
-export default GetStartedBtn
+export default connect(null, { redirectToNordea })(GetStartedBtn);
