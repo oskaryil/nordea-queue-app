39961184677e31640040685c2aae0b6c71c06bc9
Oskar Yildiz <yildizoskar@gmail.com>
2017-11-25 15:05:43 +0200

    added config variables

---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+const defaultConfig = {
+	nordea_client_id: "7917f4cc-82ea-43bd-a437-922fef2ea599",
+	nordea_redirect_uri: 'http://localhost:8080/nordeacallback'
+};
+
+export default defaultConfig;
